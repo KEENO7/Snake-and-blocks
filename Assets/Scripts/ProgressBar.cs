c3f499618dd0930df3e29260ce751098fe6c9c87
KEENO7 <nikolai.matweev2012@yandex.ru>
2022-10-05 19:12:53 +0500

    progress bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public GameObject SnakeHead;
+    public Transform FinishGrid;
+    public Slider Slider;
+
+    private float _startZ;
+    public float MaximumReachedZ;
+    public float AcceptableFinishPlayerDistance = 1f;
+
+    private void Start()
+    {
+        _startZ = SnakeHead.transform.position.z;
+    }
+
+    private void Update()
+    {
+        MaximumReachedZ = Mathf.Max(MaximumReachedZ, SnakeHead.transform.position.z);
+        float finishZ = FinishGrid.position.z;
+        float t = Mathf.InverseLerp(_startZ, finishZ + AcceptableFinishPlayerDistance, MaximumReachedZ);
+        Slider.value = t;
+    }
+}
